d40fec1c3c8106a4d9c5092dded83063b676e3fe
Mohamed <7mohamedakram@gmail.com>
2022-04-22 04:30:49 +0200

    Add mathematics logic and DOM elements

---
+++ main.js
@@ -0,0 +1,51 @@
+const sum = function (...num) {
+    return num.reduce((total, current) => total + current
+)};
+
+const subtract = function (...num) {
+    return num.reduce((total, current) => total - current
+)};
+
+const multiply = function (...num) {
+    return num.reduce((total, current) => total * current
+)};
+
+const divide = function (...num) {
+    return num.reduce((total, current) => total / current
+)};
+
+const operate = function (operator, a, b) {
+    switch (operator) {
+        case '+':
+            return sum(a,b);
+        case '-':
+            return subtract(a,b);
+        case '*':
+            return multiply(a,b);
+        case '/':
+            return divide(a, b);
+    }
+};
+
+const btn = document.querySelectorAll('.btn');
+const display = document.querySelector('#display');
+const ArithOperators = document.querySelectorAll('.operator');
+const equalOperator = document.querySelector('#equal');
+
+let displayValue = {};
+
+const createDisplayValue = (e => {
+    displayValue = e.target.textContent;
+    
+    display.textContent = displayValue;
+});
+
+btn.forEach(button => button.addEventListener('click', createDisplayValue));
+
+const getFirstNumber = (e => {
+    console.log(e.target.textContent);
+
+})
+
+ArithOperators.forEach(operator => operator.addEventListener('click', getFirstNumber));
+
